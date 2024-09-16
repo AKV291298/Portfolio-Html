166ffd0359698ba6330b32849d0720ff0f34c64e
ARJUN VERMA <96574534+AKV291298@users.noreply.github.com>
2024-09-16 08:44:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/**
+ * element toggle function
+ */
+
+
+
+
+
+/**
+ * navbar variables
+ */
+
+// const toggle = document.querySelector(["set-betn"])
+
+const setToggle = document.getElementById("ham-btn");
+
+
+
+
+
+// const navbar = document.querySelector("[data-navbar]");
+// const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+// const overlay = document.querySelector("[data-overlay]");
+
+// const navElemArr = [navToggleBtn, overlay];
+
+// for (let i = 0; i < navElemArr.length; i++) {
+
+//   navElemArr[i].addEventListener("click", function () {
+//     elemToggleFunc(navbar);
+//     elemToggleFunc(overlay);
+//   });
+
+// }
+
+alert ('Welcome to Fullstack Journey')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
